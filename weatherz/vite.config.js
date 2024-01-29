cb40bec33e42fda7946cb70c01823f5a2165c084
HadiNasution <hadinasution26@gmail.com>
2024-01-29 07:40:03 +0700

    update for deoployment

--- weatherz/vite.config.js
+++ weatherz/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-web-app/",
+  base: "/weather-web-app/weatherz/",
   plugins: [react()],
 });
